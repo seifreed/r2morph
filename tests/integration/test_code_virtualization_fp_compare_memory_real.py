f5dc5de4b2d66286a950f466a671fac0d9fd694c
Marc R. <mriverolopez@gmail.com>
2026-09-01 06:06:16 +0200

    Disassemble injected VM section on FP crash

--- tests/integration/test_code_virtualization_fp_compare_memory_real.py
+++ tests/integration/test_code_virtualization_fp_compare_memory_real.py
@@ -291,7 +291,7 @@ def test_vex_scalar_fp_compare_callee_with_native_caller_preserves_result(tmp_pa
     transformed_result = run_command([mutated], timeout=30)
     if transformed_result.returncode not in {0, original_result.returncode}:
         disassembly = run_command(
-            ["objdump", "-d", "--start-address=0x407d00", "--stop-address=0x407e20", mutated],
+            ["objdump", "-D", "--start-address=0x407d00", "--stop-address=0x407e20", mutated],
             timeout=30,
         )
         expect(
