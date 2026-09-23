1077231652c812ad3a25c2de31cdd3329ff0a3d9
Marc R. <mriverolopez@gmail.com>
2026-09-23 22:42:12 +0200

    Pin CA bundle for radare2 builds

--- .github/scripts/install_radare2.sh
+++ .github/scripts/install_radare2.sh
@@ -7,6 +7,19 @@ readonly source_url="https://github.com/radareorg/radare2.git"
 readonly destination="/tmp/radare2"
 readonly max_attempts=3
 
+configure_git_ca_bundle() {
+    local ca_bundle
+    for ca_bundle in /etc/ssl/certs/ca-certificates.crt /etc/ssl/cert.pem; do
+        if [[ -r "$ca_bundle" ]]; then
+            export GIT_SSL_CAINFO="$ca_bundle"
+            git config --global http.sslCAInfo "$ca_bundle"
+            return
+        fi
+    done
+}
+
+configure_git_ca_bundle
+
 rm -rf "$destination"
 
 cloned=false
